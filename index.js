3721ec0ac0df49e9a85d6090baf69f072a5bf272
Hrothnar <hrothnar@hotmail.com>
2023-12-15 23:21:09 +0400

    Four

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require("express");
-const log = require("./app/util/util").log;
 const homeController = require("./app/controller/home_controller");
+const expressLayout = require("express-ejs-layouts");
 
 const app = express();
 
 const port = 3000;
 
-// app.use((request, response, next) => {
-//     console.log("SOME INFO")
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log('Time:', Date.now())
-//     next();
-// });
-
 app.use(express.urlencoded({
     extended: false
 }));
 
+app.set("port", 3000);
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/app/view");
+
 app.use(express.json());
+app.use(expressLayout);
 
 app.get("/", homeController.helloWorld);
 app.get("/item/:vegetable", homeController.sendRequestParam);
+app.get("/name/:name", homeController.sendName);
 app.post("/", homeController.logBody);
 
 app.listen(port, () => {
-    console.log(`The express.js server has started and is listening on port: ${port}`);
+    console.log(`The express.js server has started and is listening on port: ${app.get("port")}`);
 });
