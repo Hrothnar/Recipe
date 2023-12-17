6c705766ed0d31f02780f309d90d2072335ecb1f
Hrothnar <hrothnar@hotmail.com>
2023-12-17 19:48:28 +0400

    Five

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const homeController = require("./app/controller/home_controller");
 const expressLayout = require("express-ejs-layouts");
+const errorController = require("./app/controller/error_controller");
+const e = require("express");
 
 const app = express();
 
@@ -21,6 +23,10 @@ app.get("/", homeController.helloWorld);
 app.get("/item/:vegetable", homeController.sendRequestParam);
 app.get("/name/:name", homeController.sendName);
 app.post("/", homeController.logBody);
+
+// app.use([errorController.status404, errorController.status500]);
+app.use(errorController.status404);
+app.use(errorController.status500);
 
 app.listen(port, () => {
     console.log(`The express.js server has started and is listening on port: ${app.get("port")}`);
